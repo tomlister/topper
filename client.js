c07d592999eaade1364148285667e0b5abbb5b67
Tom Lister <tomlister@users.noreply.github.com>
2018-04-03 21:43:36 +1000

    Fixed networking, and added a health bar.

--- client.js
+++ client.js
@@ -10,11 +10,12 @@ function main() {
 	var messages = [];
 	var player = {};
 	var token = "";
+	var overlay = [];
 	var client;
 	process.stdin.on('keypress', function (chunk, key) {
 	  if (key.name == "up" || key.name == "down" || key.name == "left" || key.name == "right") {
 	  	var send_json = {"type":"playermove","token":token,"data":key.name};
-	  	client.write(JSON.stringify(send_json));
+	  	client.write(JSON.stringify(send_json)+";");
 	  } else if (key.name == "c") {
 	  	lib.reset();
 	  	process.exit();
@@ -35,6 +36,27 @@ function main() {
 	  	}
 	  }*/
 	});
+function overlaygen(input) {
+	//populate overlay with empty arrays on the y axis
+	for (var i = 0; i < world.length; i++) {
+		overlay[i] = [];
+	}
+	for (var k in input) {
+	    if (input.hasOwnProperty(k)) {
+	    	if (k != token) {
+	    		overlay[input[k].y] = [];
+	    		overlay[input[k].y][input[k].x] = input[k].renderable;
+	    	}
+	    }
+	}
+}
+function healthrender(amt) {
+	var ren = "";
+	for (var i = 0; i < amt; i++) {
+		ren += "█";
+	}
+	return ren;
+}
 function render(callback) {
 		lib.reset();
 		var i, j;
@@ -42,16 +64,44 @@ function render(callback) {
 	      for ( j = 0; j < world[0].length; j++ ) {
 	      	if (player.x == j && player.y == i) {
 	      		lib.printf("\x1b[31m@\x1b[0m");
-	      	} else {
-	      		if (tiles[world[i][j]].is_tile == true) {
+	      	} else if (overlay.length > 0) {
+		      		if (overlay[i][j] != undefined || overlay[i][j] != null) {
+		      			lib.printf(overlay[i][j]);
+		      		} else if (tiles[world[i][j]].is_tile == true) {
 	      			lib.printf(tiles[world[i][j]].colour+world[i][j]+"\x1b[0m");
 	      		} else {
-	      			lib.printf(world[i][j]);
-	      		}
-	      	}
+		      			lib.printf(world[i][j]);
+		      		}
+	      	} else if (tiles[world[i][j]].is_tile == true) {
+	      			lib.printf(tiles[world[i][j]].colour+world[i][j]+"\x1b[0m");
+	      	} 
 	      }
 	      lib.printf("\n");
 	   	}
+	   	lib.printf("Health\n");
+	   	lib.printf("\x1b[31m");
+	   	if (player.health < 10) {
+	   		lib.printf(healthrender(1));
+	   	} else if (player.health < 20) {
+	   		lib.printf(healthrender(2));
+	   	} else if (player.health < 30) {
+	   		lib.printf(healthrender(3));
+	   	} else if (player.health < 40) {
+	   		lib.printf(healthrender(4));
+	   	} else if (player.health < 50) {
+	   		lib.printf(healthrender(5));
+	   	} else if (player.health < 60) {
+	   		lib.printf(healthrender(6));
+	   	} else if (player.health < 70) {
+	   		lib.printf(healthrender(7));
+	   	} else if (player.health < 80) {
+	   		lib.printf(healthrender(8));
+	   	} else if (player.health < 90) {
+	   		lib.printf(healthrender(9));
+	   	} else if (player.health <= 100) {
+	   		lib.printf(healthrender(10));
+	   	}
+	   	lib.printf("\x1b[0m\n")
 	   	for (var i = 0; i < messages.length; i++) {
 	   		lib.printf(messages[i])
 	   	}
@@ -60,34 +110,51 @@ function render(callback) {
 	   		callback();
 	   	}
 	}
-	client = net.createConnection({ port: 7878 }, () => {
+	client = net.createConnection({ port: 7878, host:"10.0.153.91" }, () => {
 	  var handshake_json = {"type":"handshake"}
-	  client.write(JSON.stringify(handshake_json));
+	  client.write(JSON.stringify(handshake_json)+";");
 	});
 	client.on('data', (data) => {
-		var jsondata = JSON.parse(data);
-		if (jsondata.type == "handshake") {
-			token = jsondata.token;
-			//get the world
-			var getworld_json = {"type":"getworld","token":token};
-	  		client.write(JSON.stringify(getworld_json));
-		} else if (jsondata.type == "world") {
-			//set the world
-			world = jsondata.data;
-			//get tiles
-			var gettiles_json = {"type":"gettiles","token":token};
-	  		client.write(JSON.stringify(gettiles_json));
-		} else if (jsondata.type == "tiles") {
-			//set tiles
-			tiles = jsondata.data;
-			//get initplayer
-			var getinitplayer_json = {"type":"getinitplayer","token":token};
-	  		client.write(JSON.stringify(getinitplayer_json));
-		} else if (jsondata.type == "player") {
-			//set player
-			player = jsondata.data;
-			//render
-			render();
+		var sp = data.toString()
+		sp = sp.split(";")
+		if (sp[1] == '') {
+			sp.splice(1, 1);
+		}
+			for (var i = 0; i < sp.length; i++) {
+				var jsondata = "";
+				if (sp[i] != '') {
+					jsondata = JSON.parse(sp[i]);
+				} else {
+					break;
+				}
+				if (jsondata.type == "handshake") {
+					token = jsondata.token;
+					//get the world
+					var getworld_json = {"type":"getworld","token":token};
+			  		client.write(JSON.stringify(getworld_json)+";");
+				} else if (jsondata.type == "world") {
+					//set the world
+					world = jsondata.data;
+					//get tiles
+					var gettiles_json = {"type":"gettiles","token":token};
+			  		client.write(JSON.stringify(gettiles_json)+";");
+				} else if (jsondata.type == "tiles") {
+					//set tiles
+					tiles = jsondata.data;
+					//get initplayer
+					var getinitplayer_json = {"type":"getinitplayer","token":token};
+			  		client.write(JSON.stringify(getinitplayer_json)+";");
+				} else if (jsondata.type == "player") {
+					//set player
+					player = jsondata.data;
+					//render
+					render();
+				} else if (jsondata.type == "overlaydata") {
+					//set player
+					overlaygen(jsondata.data)
+					//render
+					render();
+			}
 		}
 	});
 	client.on('end', () => {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const net = require('net');
 var players = {};
+var clients = [];
 var tiles = {
 	"~": {
 		colour:"\x1b[34m",
@@ -17,17 +18,32 @@ var tiles = {
 		colour:"",
 		type:"air",
 		is_tile:true
+	}, "●": {
+		colour:"",
+		type:"stone",
+		is_tile:true
 	}
 }
 var world = [
-	["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
-	["#", ".", ".", ".", ".", ".", ".", "~", "~", "~", "~", "~", "~", "~", "#"],
-	["#", ".", ".", ".", ".", ".", ".", "#", "#", "#", "#", "#", "#", "~", "#"],
-	["#", ".", ".", ".", ".", ".", ".", "#", " ", " ", " ", " ", "#", "~", "#"],
-	["#", ".", ".", ".", ".", ".", ".", "#", " ", " ", " ", " ", "#", "~", "#"],
-	["#", ".", ".", ".", ".", ".", ".", "#", " ", " ", " ", " ", "#", "~", "#"],
-	["#", ".", ".", ".", ".", ".", ".", "#", " ", " ", " ", " ", "#", "~", "#"],
-	["#", "#", "#", "#", "#", "#", "#", "#", " ", " ", " ", " ", "#", "#", "#"],
+	[" ","#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+	[" ","#", ".", ".", ".", ".", ".", ".", "~", "~", "~", "~", "~", "~", "~", "#", " ", "#"],
+	[" ","#", ".", ".", ".", ".", ".", ".", "#", "#", "#", "#", "#", "#", "~", "#", " ", "#"],
+	[" ","#", ".", ".", ".", ".", ".", ".", "#", " ", "●", " ", " ", "#", "~", "#", " ", "#"],
+	[" ","#", ".", ".", ".", ".", ".", ".", "#", " ", "●", " ", " ", "#", "~", "#", " ", "#"],
+	[" ","#", ".", ".", ".", ".", ".", ".", "#", " ", "●", " ", " ", "#", "~", "#", " ", "#"],
+	[" ","#", ".", ".", ".", ".", ".", ".", "#", " ", "●", " ", " ", "#", "~", "#", " ", "#"],
+	["#","#", "#", "#", "#", "#", "#", "#", "#", " ", "●", " ", " ", "#", "~", "#", " ", "#"],
+	["#"," ", " ", " ", " ", " ", " ", " ", " ", " ", "●", " ", " ", "~", "●", "~", " ", "#"],
+	["#"," ", "●", "●", "●", "●", "●", "●","●", "●", "●", " ", " ", " ", "●", " ", " ", "#"],
+	["#"," ", " ", " ", " ", " ", " ", " ", " ", " ", "●", " ", " ", " ", "●", " ", " ", "#"],
+	["#"," ", " ", " ", " ", " ", " ", " ", " ", " ", "●", " ", " ", " ", "●", " ", " ", "#"],
+	["#"," ", " ", " ", " ", " ", " ", " ", " ", " ", "●", "●", "●", "●", "●", " ", " ", "#"],
+	["#"," ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#"],
+	["#"," ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#"],
+	["#"," ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#"],
+	["#"," ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#"],
+	["#"," ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#"],
+	["#","#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
 ];
 
 function genid() {
@@ -45,64 +61,98 @@ function collision(attemptx, attempty) {
 		return false;
 	}
 }
+
+function overlaydatagen(jsondata) {
+	var overlay_json = {"type":"overlaydata","data":players};
+	clients.forEach(function (client) {
+  		client.write(JSON.stringify(overlay_json)+";");
+	});
+}
+
 const server = net.createServer((c) => {
   console.log('Client connected');
+  clients.push(c);
   c.on('end', () => {
     console.log('Client disconnected');
   });
   c.on('data', (data) => {
-  	var jsondata = JSON.parse(data);
-  	if (jsondata.type == "handshake") {
-  		var gid = genid();
-  		var connect_json = {"type":"handshake", "token":gid};
-  		players[gid] = {"valid":true};
-  		c.write(JSON.stringify(connect_json));
-  	} else if (players[jsondata.token]) {
-  		if (jsondata.type == "playermove") {
-	  		if (jsondata.data == "up") {
-	  			if (!collision(players[jsondata.token].x, players[jsondata.token].y - 1)) {
-					players[jsondata.token].y -= 1;
-					var player_json = {"type":"player","data":players[jsondata.token]};
-	  				c.write(JSON.stringify(player_json));
+  	var sp = data.toString()
+	sp = sp.split(";")
+	if (sp[1] == '') {
+		sp.splice(1, 1);
+	}
+	for (var i = 0; i < sp.length; i++) {
+		var jsondata = "";
+		if (sp[i] != '') {
+			jsondata = JSON.parse(sp[i]);
+		}
+	  	if (jsondata.type == "handshake") {
+	  		var gid = genid();
+	  		var connect_json = {"type":"handshake", "token":gid};
+	  		players[gid] = {"valid":true};
+	  		c.write(JSON.stringify(connect_json)+";");
+	  	} else if (players[jsondata.token]) {
+	  		if (jsondata.type == "playermove") {
+		  		if (jsondata.data == "up") {
+		  			if (!collision(players[jsondata.token].x, players[jsondata.token].y - 1)) {
+						players[jsondata.token].y -= 1;
+						var player_json = {"type":"player","data":players[jsondata.token]};
+		  				c.write(JSON.stringify(player_json)+";");
+					}
+				} else if (jsondata.data == "down") {
+					if (!collision(players[jsondata.token].x, players[jsondata.token].y + 1)) {
+						players[jsondata.token].y += 1;
+						var player_json = {"type":"player","data":players[jsondata.token]};
+		  				c.write(JSON.stringify(player_json)+";");
+					}
+	  			} else if (jsondata.data == "left") {
+					if (!collision(players[jsondata.token].x - 1, players[jsondata.token].y)) {
+						players[jsondata.token].x -= 1;
+						var player_json = {"type":"player","data":players[jsondata.token]};
+		  				c.write(JSON.stringify(player_json)+";");
+					}
+	  			} else if (jsondata.data == "right") {
+					if (!collision(players[jsondata.token].x + 1, players[jsondata.token].y)) {
+						players[jsondata.token].x += 1;
+						var player_json = {"type":"player","data":players[jsondata.token]};
+		  				c.write(JSON.stringify(player_json)+";");
+					}
+	  			} 
+	  			overlaydatagen();
+	  		} else if (jsondata.type == "getworld") {
+	  			var world_json = {"type":"world","data":world};
+	  			c.write(JSON.stringify(world_json)+";");
+	  		} else if (jsondata.type == "gettiles") {
+	  			var tiles_json = {"type":"tiles","data":tiles};
+	  			c.write(JSON.stringify(tiles_json)+";");
+	  		} else if (jsondata.type == "getinitplayer") {
+	  			players[jsondata.token] = {
+	  				"x":5, 
+	  				"y":5, 
+	  				"wet": false, 
+	  				"drying": false,
+	  				"health": 100,
+	  				"renderable": "@",
+					setWet: function (status) {
+						wet = status;
+						//messages.push("Oh no I'm wet!\n");
+					}
 				}
-			} else if (jsondata.data == "down") {
-				if (!collision(players[jsondata.token].x, players[jsondata.token].y + 1)) {
-					players[jsondata.token].y += 1;
-					var player_json = {"type":"player","data":players[jsondata.token]};
-	  				c.write(JSON.stringify(player_json));
+	  			var player_json = {"type":"player","data":players[jsondata.token]};
+	  			c.write(JSON.stringify(player_json)+";");
+	  			overlaydatagen();
+	  		} else if (jsondata.type == "getoverlay") {
+				var overlaydata = players;
+				for (var k in overlaydata) {
+					if (overlaydata.hasOwnProperty(k)) {
+				        if (k == jsondata.token) {
+				        	delete overlaydata[k];
+				        }
+				    }
 				}
-  			} else if (jsondata.data == "left") {
-				if (!collision(players[jsondata.token].x - 1, players[jsondata.token].y)) {
-					players[jsondata.token].x -= 1;
-					var player_json = {"type":"player","data":players[jsondata.token]};
-	  				c.write(JSON.stringify(player_json));
-				}
-  			} else if (jsondata.data == "right") {
-				if (!collision(players[jsondata.token].x + 1, players[jsondata.token].y)) {
-					players[jsondata.token].x += 1;
-					var player_json = {"type":"player","data":players[jsondata.token]};
-	  				c.write(JSON.stringify(player_json));
-				}
-  			} 
-  		} else if (jsondata.type == "getworld") {
-  			var world_json = {"type":"world","data":world};
-  			c.write(JSON.stringify(world_json));
-  		} else if (jsondata.type == "gettiles") {
-  			var tiles_json = {"type":"tiles","data":tiles};
-  			c.write(JSON.stringify(tiles_json));
-  		} else if (jsondata.type == "getinitplayer") {
-  			players[jsondata.token] = {
-  				"x":5, 
-  				"y":5, 
-  				"wet": false, 
-  				"drying": false,
-				setWet: function (status) {
-					wet = status;
-					//messages.push("Oh no I'm wet!\n");
-				}
-			}
-  			var player_json = {"type":"player","data":players[jsondata.token]};
-  			c.write(JSON.stringify(player_json));
+	  			var overlay_json = {"type":"overlaydata","data":overlaydata};
+	  			c.write(JSON.stringify(overlay_json)+";");
+	  		}
   		}
   	}
   });
