293ddb776ece676df3486ead029ecf9fcb84e0a9
Tom Lister <tomlister@users.noreply.github.com>
2018-04-10 14:13:25 +1000

    Multicast Libraries

---
+++ libs/broadcast.js
@@ -0,0 +1,36 @@
+var port = 7879;
+var mcastaddr = "230.120.120.120";
+var data = {ip: "", message:"Topper Server"}
+var dgram = require('dgram');
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var server = dgram.createSocket({ type: 'udp4', reuseAddr: true }); 
+server.bind(port, function(){
+    server.setBroadcast(true);
+    server.setMulticastTTL(128);
+    server.addMembership(mcastaddr);
+});
+function broadcast() {
+	Object.keys(ifaces).forEach(function (ifname) {
+	  var alias = 0;
+	  ifaces[ifname].forEach(function (iface) {
+	    if ('IPv4' !== iface.family || iface.internal !== false) {
+	      return;
+	    }
+	    if (alias >= 1) {
+	    } else {
+	      if (ifname = "en0") {
+	      	data.ip = iface.address;
+	      	setInterval(broadcastf, 3000);
+	      }
+	    }
+	    ++alias;
+	  });
+	});
+}
+function broadcastf() {
+    var message = new Buffer(JSON.stringify(data));
+    server.send(message, 0, message.length, port, mcastaddr);
+    console.log("Sent " + message + " to multicast address");
+}
+module.exports.broadcast = broadcast;
---
+++ libs/broadcast_recv.js
@@ -0,0 +1,34 @@
+var port = 7879;
+var mcastaddr = "230.120.120.120";
+var host = "";
+var dgram = require('dgram');
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var client = dgram.createSocket({ type: 'udp4', reuseAddr: true })
+function search() {
+	client.on('message', function (message, remote) {   
+    	return message;
+	});
+	Object.keys(ifaces).forEach(function (ifname) {
+	  var alias = 0;
+	  ifaces[ifname].forEach(function (iface) {
+	    if ('IPv4' !== iface.family || iface.internal !== false) {
+	      return;
+	    }
+	    if (alias >= 1) {
+	    } else {
+	      if (ifname = "en0") {
+	      	host = iface.address;
+	      	client.bind(port, function () {
+				client.addMembership(mcastaddr, host);
+				var address = client.address();
+				client.setBroadcast(true)
+				client.setMulticastTTL(128); 
+			});
+	      }
+	    }
+	    ++alias;
+	  });
+	});
+}
+module.exports.search = search;
